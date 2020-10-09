af2fec3a9d872ea59595f523712f67772b18a417
DESKTOP-6UC2301\Thomas <thomas.vdhoeven@hotmail.com>
2020-10-09 14:13:41 +0200

    remove constructors

--- src/main/java/nl/han/ica/oose/dea/spotitube/exceptions/ApplicationException.java
+++ src/main/java/nl/han/ica/oose/dea/spotitube/exceptions/ApplicationException.java
@@ -5,13 +5,7 @@ import java.io.Serializable;
 public class ApplicationException extends Exception implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    public ApplicationException() {
-        super();
-    }
     public ApplicationException(String msg)   {
         super(msg);
-    }
-    public ApplicationException(String msg, Exception e)  {
-        super(msg, e);
     }
 }
