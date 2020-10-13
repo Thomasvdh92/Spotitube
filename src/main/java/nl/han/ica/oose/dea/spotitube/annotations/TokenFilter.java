a68c6d543d1a4f36ce5049dd0297e5fc9f5e86c5
DESKTOP-6UC2301\Thomas <thomas.vdhoeven@hotmail.com>
2020-10-13 10:37:40 +0200

    Add @TokenRequired annotation to check for token

---
+++ src/main/java/nl/han/ica/oose/dea/spotitube/annotations/TokenFilter.java
@@ -0,0 +1,33 @@
+package nl.han.ica.oose.dea.spotitube.annotations;
+
+import nl.han.ica.oose.dea.spotitube.datasource.ITokenDAO;
+import nl.han.ica.oose.dea.spotitube.domain.Token;
+import nl.han.ica.oose.dea.spotitube.exceptions.ApplicationException;
+import nl.han.ica.oose.dea.spotitube.exceptions.EntityNotFoundException;
+
+import javax.annotation.Priority;
+import javax.inject.Inject;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@TokenRequired
+@Priority(Priorities.AUTHENTICATION)
+public class TokenFilter implements ContainerRequestFilter {
+
+    @Inject
+    ITokenDAO tokenDAO;
+
+    @Override
+    public void filter(ContainerRequestContext containerRequestContext) {
+        String tokenString = containerRequestContext.getUriInfo().getQueryParameters().getFirst("token");
+        try {
+            tokenDAO.read(tokenString);
+        } catch (EntityNotFoundException e) {
+            containerRequestContext.abortWith(Response.status(Response.Status.FORBIDDEN).build());
+        }
+    }
+}
---
+++ src/main/java/nl/han/ica/oose/dea/spotitube/annotations/TokenRequired.java
@@ -0,0 +1,13 @@
+package nl.han.ica.oose.dea.spotitube.annotations;
+
+import javax.ws.rs.NameBinding;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@NameBinding
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface TokenRequired {
+}
